f6c6aaeffd22082fe10d83821657d21306aaff8a
Nuno Coelho Santos <nunocs@fb.com>
2022-08-15 10:44:16 +0100

    Fix gestures view

--- Components/Gestures.swift
+++ Components/Gestures.swift
@@ -16,46 +16,62 @@ struct Gestures: View {
     
     var body: some View {
         
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .cornerRadius(10)
-            .offset(cardDragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged {
-                        cardDragAmount = $0.translation
-                    }
-                    .onEnded { _ in
-                        withAnimation {
-                            cardDragAmount = .zero
+        List {
+            
+            Section {
+                
+                VStack {
+                    LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .frame(width: 300, height: 200)
+                        .cornerRadius(10)
+                        .offset(cardDragAmount)
+                        .gesture(
+                            DragGesture()
+                                .onChanged {
+                                    cardDragAmount = $0.translation
+                                }
+                                .onEnded { _ in
+                                    withAnimation {
+                                        cardDragAmount = .zero
+                                    }
+                                }
+                        )
+                    
+                    /* HStack(spacing: -1) {
+                        ForEach(0..<letters.count) { number in
+                            Text(String(letters[number]))
+                                .padding(5)
+                                .font(.title)
+                                .foregroundColor(lettersEnabled ? .blue : .red)
+                                .offset(lettersDragAmount)
+                                .animation(
+                                    .default.delay(Double(number) / 20),
+                                    value: lettersDragAmount
+                                )
                         }
                     }
-            )
-        
-        HStack(spacing: -1) {
-            ForEach(0..<letters.count) { number in
-                Text(String(letters[number]))
-                    .padding(5)
-                    .font(.title)
-                    .foregroundColor(lettersEnabled ? .blue : .red)
-                    .offset(lettersDragAmount)
-                    .animation(
-                        .default.delay(Double(number) / 20),
-                        value: lettersDragAmount
-                    )
-            }
-        }
-        .gesture(
-            DragGesture()
-                .onChanged {
-                    lettersDragAmount = $0.translation
+                    .gesture(
+                        DragGesture()
+                            .onChanged {
+                                lettersDragAmount = $0.translation
+                            }
+                            .onEnded { _ in
+                                lettersDragAmount = .zero
+                                lettersEnabled.toggle()
+                            }
+                    ) */
                 }
-                .onEnded { _ in
-                    lettersDragAmount = .zero
-                    lettersEnabled.toggle()
-                }
-        )
+                .frame(maxWidth: .infinity, minHeight: 300)
 
+                
+            } header: {
+                Text("Drag gesture")
+            } footer: {
+                Text("Here we drag a credit card shaped rectagle that moves (translates its position) as a drag gesture stars, and returns to the initial position with animaiton when drag ends.")
+            }
+            
+        }
+        
     }
 }
 
